91c0d7a09b5575137a1e18806cd9a1c4d538c236
Antu <122485403+antukung@users.noreply.github.com>
2024-01-01 16:24:23 -0300

    aca esta

---
+++ javascript/constructorlibros.js
@@ -0,0 +1,45 @@
+const divLibros = document.querySelector(".libros-argentina");
+const inputLibros = document.querySelector("#input-nombre");
+
+function crearLibros(lista) {
+  divLibros.innerHTML = ""; // Limpiar resultados anteriores
+
+  for ( i = 0; i < lista.length; i++) {
+    const div = document.createElement("div");
+    div.classList.add("libro");
+
+    const nombre = document.createElement("p");
+    nombre.textContent = lista[i].nombre;
+    nombre.classList.add("nombreDelLibro");
+
+    const enlaceImagen = document.createElement("a");
+    enlaceImagen.href = lista[i].enlace;
+    enlaceImagen.target = "_blank";
+
+    const imagen = document.createElement("img");
+    imagen.src = lista[i].imagen;
+    imagen.classList.add("imagenDelLibro");
+
+    enlaceImagen.appendChild(imagen);
+    div.appendChild(enlaceImagen);
+    div.appendChild(nombre);
+
+    divLibros.appendChild(div);
+  }
+}
+
+crearLibros(databaselibrosJSON);
+
+function buscadorDeNombre() {
+  const nombreLeido = inputLibros.value.toLowerCase();
+  divLibros.innerHTML = "";
+
+  if (nombreLeido.trim() !== "") {
+    const filtrados = databaselibrosJSON.filter(libro => libro.nombre.toLowerCase().includes(nombreLeido));
+    crearLibros(filtrados);
+  } else {
+    crearLibros(databaselibrosJSON);
+  }
+}
+
+
