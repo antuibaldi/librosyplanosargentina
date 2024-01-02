3bac3225327ed41d9b80b7c5ebd6813d27c1514c
Antu <122485403+antukung@users.noreply.github.com>
2024-01-01 22:14:20 -0300

    aca esta

---
+++ javascript/app2planos.js
@@ -0,0 +1,26 @@
+const databasereseniaslibrosJSON= [
+    {  
+      "_id": 1,
+      "imagenResenia":"",
+      "resenia":"Casa de diseño soler",
+      "texto":"Esta casa consta de un diseño estructural sobre una grilla de 3 metros por 3 metros y un enparrillado de vigas"
+    
+    
+    },
+    {  
+        "_id": 2,
+        "imagen-resenia":"",
+        "resenia":"",
+        "texto":""
+      
+      
+    },
+    {  
+        "_id": 3,
+        "imagen-resenia":"",
+        "resenia":"",
+        "texto":""
+      
+      
+    }   
+]
---
+++ javascript/appplanos.js
@@ -0,0 +1,343 @@
+const databaselibrosJSON= [
+   /* {  
+      "_id": 1,
+      "nombre":"Harry Poter y La Piedra Filosofal",
+      "imagen":"../img/Libro1.png",
+      "enlace":"https://mega.nz/file/d0hFDDwD#rcw8tcK1U0gTyqgqt0-JLIqeTeAv4r-vQu-_044_9sc"
+    },
+    { 
+      "_id": 2,
+      "nombre":"Harry Poter y La Camara Secreta",
+      "imagen":"../img/Libro2.png",
+      "enlace":"https://mega.nz/file/RhRDFaTD#SIjrObGh9IhugeAAjPnHFRsIcfaFGLfmHmxrpiei0mU"
+    },
+    { 
+      "_id": 3,
+      "nombre":"Harry Poter y El Prisionero de Azkaban",
+      "imagen":"../img/Libro3.png",
+      "enlace":"https://mega.nz/file/p0BhhBwR#znYOs5peuCcMeOUNd55OqkJ05mmjWJes6aVgICk16NU"
+    }
+    ,
+    { 
+      "_id": 4,
+      "nombre":"Harry Poter y El Caliz del Fuego",
+      "imagen":"../img/Libro4.png",
+      "enlace":"https://mega.nz/file/xhJxibgL#_5NWIlWEHvYhu3FpqYXzGWe_5pUJ_-yCAJBxOWS_ngc"
+    }
+    ,
+    { 
+      "_id": 5,
+      "nombre":"Harry Poter y La Orden del Fenix",
+      "imagen":"../img/Libro5.png",
+      "enlace":"https://mega.nz/file/x8ggASRJ#4b9WE1OmfPFx23EDo6qTkU50wPBL11zaUDWCU9OzGg4"
+    }
+    ,
+    { 
+      "_id": 6,
+      "nombre":"Harry Poter y El Principe Mestizo",
+      "imagen":"../img/Libro6.png",
+      "enlace":"https://mega.nz/file/EwYkWICb#FrTShfDrkA17RxV0xmdQ_t3RFHbc3uv90fw24KPT16E"
+    }
+    ,
+    { 
+      "_id": 7,
+      "nombre":"Harry Poter y Las Reliquias de la Muerte ",
+      "imagen":"../img/Libro7.png",
+      "enlace":"https://mega.nz/file/tk4hBJ6Z#ntRJKC5VmPlw_i17KxobdIf7jjY6T1Y9G_Zo20seDH8"
+    }
+    ,
+    { 
+      "_id": 8,
+      "nombre":"Animales Fantasticos y Donde Encontrarlos",
+      "imagen":"../img/Libro8.png",
+      "enlace":"https://mega.nz/file/x5wihQhA#CbaMfr8ij5Sf0-7Q4BrnDLeRZ3p-7yoPw-S1Mu9dg34"
+    }
+    ,
+    { 
+      "_id": 9,
+      "nombre":"James Poter y La Encrucijada de Los Mayores",
+      "imagen":"../img/Libro9.png",
+      "enlace":"https://mega.nz/file/hhgm2A7B#0FYe-iGwroL_wO9-4bTPb5cuoqRX4K0c9xJu7336m14"
+    }
+    ,
+    { 
+      "_id": 10,
+      "nombre":"James Poter y La Maldicion del Guardian",
+      "imagen":"../img/Libro10.png",
+      "enlace":"https://mega.nz/file/NxoH1DxL#9OLmR-_VxkhJH_8GW5x0RqINDtGrUSX5CS0XkgibPoM"
+    }
+    ,
+    { 
+      "_id": 11,
+      "nombre":"James Poter y La Bobeda de los destinos",
+      "imagen":"../img/Libro11.png",
+      "enlace":"https://mega.nz/file/Bx5y2YLT#2dts9I4Pe9NthKdlJ5RGKQlrxJHgyio4C_x9tAoxv3Y"
+    }
+    ,
+    { 
+      "_id": 12,
+      "nombre":"James Poter y la Red Morigan",
+      "imagen":"../img/Libro12.png",
+      "enlace":"https://mega.nz/file/4t5QxAxS#oz-SL0v1wSff7ecrZ6QLQFxQxGD7rBnJrutvUoc6aKY"
+    }
+    ,
+    { 
+      "_id": 13,
+      "nombre":"James Poter y El Hilo Carmesi",
+      "imagen":"../img/Libro13.png",
+      "enlace":"https://mega.nz/file/M1oUiYgY#ZGLMrgZtDwETsXUDwgS8TWYBXJu5pb33BdflAPWkFT8"
+    }
+    ,
+    { 
+      "_id": 14,
+      "nombre":"La Primera Navidad de Harry y Otros Cuentos",
+      "imagen":"../img/Libro14.png",
+      "enlace":"https://mega.nz/file/BoAAFZDK#Z3ATAfISsJrva6OGzNbYj0fe9kGVvpRVSmpKh-_u2Ps"
+    }
+    ,
+    { 
+      "_id": 15,
+      "nombre":"La Chica del Embarcadero",
+      "imagen":"../img/Libro15.png",
+      "enlace":"https://mega.nz/file/Z8gwCAzZ#QGQWWyDlwIp3zsHo2CNH1NyCEaJ0Hl7BNuwYJQE0DWA"
+    }
+    ,
+    { 
+      "_id": 16,
+      "nombre":"Tao",
+      "imagen":"../img/Libro16.png",
+      "enlace":"https://mega.nz/file/4kgWjSrQ#_To9vMti7DUKZkstySBYbcbeDNcBxtuGsZ_KqzsN7FM"
+    }
+    ,
+    { 
+      "_id": 17,
+      "nombre":"Yo Cuento de Hada Jara Sirkin",
+      "imagen":"../img/Libro17.png",
+      "enlace":"https://mega.nz/file/45hlhDpb#WEH3sMao_yp53fzSHMCHl_JkU_uSW7CTV3_AceJCYZ0"
+    }
+    ,
+    { 
+      "_id": 18,
+      "nombre":"Herejia Bermeja",
+      "imagen":"../img/Libro18.png",
+      "enlace":"https://mega.nz/file/lthECLIC#70_tdqUBVcVlrvlHyP1oe84HNiRlMbxjdz6ElaWHgbI"
+    }
+    ,
+    { 
+      "_id": 19,
+      "nombre":"Relectura (en viaje)",
+      "imagen":"../img/Libro19.png",
+      "enlace":"https://mega.nz/file/U0JzHBpD#Hzgx8ATpew4hnnE9k0zut-HBmEZN0enzjIEDJ9ho9XA"
+    }
+    ,
+    { 
+      "_id": 20,
+      "nombre":"El Nacimiento de laa Filosofia",
+      "imagen":"../img/Libro20.png",
+      "enlace":"https://mega.nz/file/d0plnYgZ#cSNJz8EfrciDGGJOs7Vedb6Wd2PK-CxIY1FmJxYlgiI"
+    }
+    ,
+    { 
+      "_id": 21,
+      "nombre":"Los Dia Del Venado",
+      "imagen":"../img/Libro21.png",
+      "enlace":"https://mega.nz/file/Y8I2nDJR#G7h2TbF5fAFeyGyAem3hLKxFZITourVZknoTXLaxiho"
+    }
+    ,
+    { 
+      "_id": 22,
+      "nombre":"Los dias De La Sombra",
+      "imagen":"../img/Libro26.png",
+      "enlace":"https://mega.nz/file/B0wFzAIS#6BIfbSBT2s37wiIkc2iLq4m3IZANbKOI7-CXfq72I0Q"
+    }
+    ,
+    { 
+      "_id": 23,
+      "nombre":"El Coraje de La Verdad Foucault",
+      "imagen":"../img/Libro22.png",
+      "enlace":"https://mega.nz/file/ktgyTAJD#wkYwvFK6qGrGeAupTyr1QWQMfe37eBSnDX659amqcag"
+    }
+    ,
+    { 
+      "_id": 24,
+      "nombre":"El Orden del Discurso Foucault",
+      "imagen":"../img/Libro23.png",
+      "enlace":"https://mega.nz/file/xwQkyTqI#FT9ZQW6WvSZvLxaanUWesEPu_v4Zx5Vtt9oM9Ap4bKc"
+    }
+    ,
+    { 
+      "_id": 25,
+      "nombre":"Enfermedad Mental y Personalidad Foucault",
+      "imagen":"../img/Libro24.png",
+      "enlace":"https://mega.nz/file/s9ZSyTxK#oTs4mgjD7Q00gZgs9w6VOdEqvZ7czutNnIrfKAubpjo"
+    }
+    ,
+    { 
+      "_id": 26,
+      "nombre":"Relato de Una Soñadora",
+      "imagen":"../img/Libro25.png",
+      "enlace":"https://mega.nz/file/clZXEbgS#vE2hWJ2TYvYBxPQxOfwO6VcC_XmNLpI_wj6ShpPSUmA"
+    }
+    ,
+    { 
+      "_id": 27,
+      "nombre":"La Condesa Sangrienta",
+      "imagen":"../img/Libro27.png",
+      "enlace":"https://mega.nz/file/hpZGBTyY#Jiz4DqIJcVAI-68N5rzWr5BVBwSdVj4fMD8RRGm6ExY"
+    }
+    ,
+    { 
+      "_id": 28,
+      "nombre":"Planetas Morales",
+      "imagen":"../img/Libro28.png",
+      "enlace":"https://mega.nz/file/RpoTkDJR#P0hSM66yBhkR1yy0f7dH7yQ6EKNcxXi2OPYclTLNNFY"
+    }
+    ,
+    { 
+      "_id": 29,
+      "nombre":"La Palabra Dibujada",
+      "imagen":"../img/Libro29.png",
+      "enlace":"https://mega.nz/file/4t4F0BhY#e-NEIbc3Opkx5BgckX2xh4xiCUIn-dHZaomnRyV05dw"
+    }
+    ,
+    { 
+      "_id": 30,
+      "nombre":"Prosas Apatriadas Aumentadas",
+      "imagen":"../img/Libro30.png",
+      "enlace":"https://mega.nz/file/58YTmCIa#7qCvqh4-49fL363VaKqCU-9SvTXIOvQnEeVELFIZCnk"
+    }
+    ,
+    { 
+      "_id": 31,
+      "nombre":"Ramon Cote Poemas",
+      "imagen":"../img/Libro31.png",
+      "enlace":"https://mega.nz/file/80J2AbSB#Z4ePwrDnhndMh539qo-a5zxc3vVC7UIbwWbTyn9gkOI"
+    }
+    ,
+    { 
+      "_id": 32,
+      "nombre":"Sociedad dek Espectaculo",
+      "imagen":"../img/Libro32.png",
+      "enlace":"https://mega.nz/file/ppIFTCKC#54KQoEDQ52PMG6nl76ZZpUvbh1hVWbV1388kr5TXG9E"
+    }
+    
+    ,
+    { 
+      "_id": 33,
+      "nombre":"Crepusculo",
+      "imagen":"../img/Libro33.png",
+      "enlace":"https://mega.nz/file/N1ZllABD#Jm0IFYptmfW0LGs-jxi6VexYS7J0LvnNU9D37q2vLEI"
+    }
+    ,
+    { 
+      "_id": 34,
+      "nombre":"Luna Nueva",
+      "imagen":"../img/Libro34.png",
+      "enlace":"https://mega.nz/file/Eo4AxQxL#bibOdmdgiuCwwDGKKKQD39H76Qmj0q1yB5Hk16KkSck"
+    }
+    ,
+    { 
+      "_id": 35,
+      "nombre":"Eclipse",
+      "imagen":"../img/Libro35.png",
+      "enlace":"https://mega.nz/file/c9ZCSaTb#bV3WbYb_yVWNSAckvylB7gJ-pD9f5kOxiUKh-VeiG4M"
+    }
+    ,
+    { 
+      "_id": 36,
+      "nombre":"Amanacer",
+      "imagen":"../img/Libro36.png",
+      "enlace":"https://mega.nz/file/Q4QWgBDb#VAuzo2PiUuIshcVWhpNO3PgwEYJD6oF2qoi2g5Hun-g"
+    }
+    
+    ,
+    { 
+      "_id": 37,
+      "nombre":"El libro Retorcido",
+      "imagen":"../img/Libro37.png",
+      "enlace":"https://mega.nz/file/MkxTySTL#WMan4mClcv-6b51dwUl07qT5nhqQdxdVvEa4Sr267rI"
+    }
+    ,
+    { 
+      "_id": 38,
+      "nombre":"Jorgumo el Infierno de Minori Kendo",
+      "imagen":"../img/Libro38.png",
+      "enlace":"https://mega.nz/file/Bo5QhazK#rOib-l_2qcuPTtipQ47Yo9qfpLVHTLagg1T0kQ1jt9A"
+    }
+    ,
+    { 
+      "_id": 39,
+      "nombre":"Jorgumo Un Lugar Llamado Dreamsland",
+      "imagen":"../img/Libro39.png",
+      "enlace":"https://mega.nz/file/F4YTgQYR#72hZML46mgtk7jRiG3WEcTHY4KSnudUTMU8c1TuyY4o"
+    }
+    ,
+    { 
+      "_id": 40,
+      "nombre":"Jack, el Cazador",
+      "imagen":"../img/Libro40.png",
+      "enlace":"https://mega.nz/file/UxZXlJCa#O5hhelhoF8gFsUf7LeMlzQ8OoBy8qPe8UE638Yarg9E"
+    }
+    ,
+    { 
+      "_id": 41,
+      "nombre":"El Joven y la Bruja",
+      "imagen":"../img/Libro41.png",
+      "enlace":"https://mega.nz/file/NxJVzSzZ#tETAPNvWrLRhSVv3z4XFlS-cBRKhySVJR86n1A7JPzI"
+    }
+    ,
+    { 
+      "_id": 42,
+      "nombre":"La Cabra",
+      "imagen":"../img/Libro42.png",
+      "enlace":"https://mega.nz/file/JoQlzapS#e_i1P7CW9-Id_N-3j1cruJVXNtk5irD-MOkQPr_U9pk"
+    }
+    ,
+    { 
+      "_id": 43,
+      "nombre":"El Cazador",
+      "imagen":"../img/Libro43.png",
+      "enlace":"https://mega.nz/file/4oZwUCIS#mAtvMS9XqfPeDpK5yIuhlCcF24tf4bocz8Y2PtIBfkg"
+    }
+    
+    ,
+    { 
+      "_id": 44,
+      "nombre":"Cuaderno de Artes Marciales",
+      "imagen":"../img/Libro44.png",
+      "enlace":"https://mega.nz/file/koB0HCxa#dUfZSJPfQlCvA5nKOW6jrEkYr-2jhCrLzylLZX5bFd4"
+    }
+    
+    ,
+    { 
+      "_id": 37,
+      "nombre":"",
+      "imagen":"../img/Libro25.png",
+      "enlace":""
+    }
+    ,
+    { 
+      "_id": 38,
+      "nombre":"",
+      "imagen":"../img/Libro25.png",
+      "enlace":""
+    }
+    ,
+    { 
+      "_id": 39,
+      "nombre":"",
+      "imagen":"../img/Libro25.png",
+      "enlace":""
+    }
+    ,
+    { 
+      "_id": 40,
+      "nombre":"",
+      "imagen":"../img/Libro25.png",
+      "enlace":""
+    }
+  
+  
+    
+  */
+  ]
